4c8e4e3ab6897054846050ebb6bbf721dc734fe2
zuca99 <agungriziq27@gmail.com>
2022-09-16 10:22:57 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -13,6 +13,10 @@
 
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, perferendis quae impedit esse animi deleniti eligendi labore repellendus officia odit, ipsam natus cumque. Unde veniam, minima modi maiores atque error?</p>
 
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 
 </body>
 </html>
