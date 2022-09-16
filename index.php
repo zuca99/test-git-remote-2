37f6e9f6ead6218e2b2864183870d6592719cfaa
zuca99 <agungriziq27@gmail.com>
2022-09-16 10:15:57 +0700

    menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote</title>
+
+</head>
+<body>
+
+    <h1>Hello World!</h1>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, perferendis quae impedit esse animi deleniti eligendi labore repellendus officia odit, ipsam natus cumque. Unde veniam, minima modi maiores atque error?</p>
+
+
+</body>
+</html>
